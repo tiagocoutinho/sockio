644457e00e01e30cea3b44b211c45c872d5760c7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-10-16 20:35:27 +0200

    Fix pypi description

--- setup.py
+++ setup.py
@@ -18,66 +18,8 @@ if TESTING:
     setup_requirements += ['pytest-runner']
 test_requirements = ['pytest', 'pytest-cov', 'pytest-asyncio']
 
-
-description = '''\
-sockio
-======
-
-A concurrency agnostic socket library on python.
-
-So far implemented REQ-REP semantics with auto-reconnection facilites.
-
-Implementations for:
-
-* classic blocking API
-* future based API
-* asyncio
-
-Join the party by bringing your own concurrency library with a PR!
-
-I am looking in particular for implementations over trio and curio.
-
-Installation
-------------
-
-From within your favourite python environment::
-
-    pip install sockio
-
-
-Usage
------
-
-asyncio
-#######
-
-::
-
-    import asyncio
-    from sockio.aio import Socket
-
-    async def main():
-        sock = Socket('acme.example.com', 5000)
-        # Assuming a SCPI complient on the other end we can ask for:
-        reply = await sock.write_readline(b'*IDN?\n')
-        print(reply)
-
-    asyncio.run(main())
-
-
-classic
-#######
-
-::
-
-    from sockio.sio import Socket
-
-    sock = Socket('acme.example.com', 5000)
-    # Assuming a SCPI complient on the other end we can ask for:
-    reply = sock.write_readline(b'*IDN?\n')
-    print(reply)
-
-'''
+with open('README.md') as f:
+    description = f.read()
 
 setup(
     name='sockio',
