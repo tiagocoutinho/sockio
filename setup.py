5b06d3c1bb8cc8e27db57df3e0ff42e55ae3dc86
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-10-17 07:50:20 +0200

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tiagocoutinho/sockio',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=True
 )
