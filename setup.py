d6e1d29b5f2b960f49a1a80bbc0698b592892eb7
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-09-17 11:37:33 +0200

    Remove test runner from setup

--- setup.py
+++ setup.py
@@ -5,17 +5,6 @@
 import sys
 from setuptools import setup, find_packages
 
-
-TESTING = any(x in sys.argv for x in ["test", "pytest"])
-
-setup_requirements = []
-if TESTING:
-    if sys.version_info < (3, 7):
-        print("testing sockio needs python >= 3.7")
-        exit(1)
-    setup_requirements += ["pytest-runner"]
-test_requirements = ["pytest", "pytest-cov", "pytest-asyncio"]
-
 with open("README.md") as f:
     description = f.read()
 
@@ -39,9 +28,6 @@ setup(
     long_description_content_type="text/markdown",
     keywords="socket, asyncio",
     packages=find_packages(include=["sockio"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
     url="https://tiagocoutinho.github.io/sockio/",
     project_urls={
         "Documentation": "https://tiagocoutinho.github.io/sockio/",
